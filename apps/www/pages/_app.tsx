806d0582f194414ff60d4b0ff0b2150dd5dd381d
Ayaan <minsu_kim@bishanoi.net>
2024-04-27 21:34:38 +0700

    :green_heart: fix: Fix metatag warning on _document

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -3,6 +3,8 @@ import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { appWithTranslation } from "next-i18next"
 
+import Head from "next/head"
+
 import Navbar from "@packages/ui/components/Navbar"
 import Footer from "@packages/ui/components/Footer"
 import ChannelIO from "@packages/channelio/component"
@@ -10,6 +12,41 @@ import ChannelIO from "@packages/channelio/component"
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+
+        <meta
+          name="description"
+          content="NGuard Security는 디스코드 보안 서비스로, 디스코드 서버 보안에 새로운 변화를 선사합니다."
+        />
+        <meta
+          property="og:description"
+          content="NGuard Security는 디스코드 보안 서비스로, 디스코드 서버 보안에 새로운 변화를 선사합니다."
+        />
+
+        <link
+          rel="preconnect"
+          href="https://cdn.nguard.dev"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://cdn.jsdelivr.net"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          rel="shortcut icon"
+          href="https://cdn.nguard.dev/assets/common/images/favicon.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://cdn.nguard.dev/assets/common/images/favicon.png"
+        />
+      </Head>
       <Navbar domain="www" />
 
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
--- apps/www/pages/_document.tsx
+++ apps/www/pages/_document.tsx
@@ -3,40 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-        <meta
-          name="description"
-          content="NGuard Security는 디스코드 보안 서비스로, 디스코드 서버 보안에 새로운 변화를 선사합니다."
-        />
-        <meta
-          property="og:description"
-          content="NGuard Security는 디스코드 보안 서비스로, 디스코드 서버 보안에 새로운 변화를 선사합니다."
-        />
+      <Head />
 
-        <link
-          rel="preconnect"
-          href="https://cdn.nguard.dev"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://cdn.jsdelivr.net"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          rel="shortcut icon"
-          href="https://cdn.nguard.dev/assets/common/images/favicon.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="https://cdn.nguard.dev/assets/common/images/favicon.png"
-        />
-      </Head>
       <body>
         <Main />
         <NextScript />
